706c44f4217a70ea186ea30d07d27624bf06b5eb
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-11-21 18:44:49 -0400

    Adding Gary plugin

---
+++ cli/plugins/gary/about.sh
@@ -0,0 +1,12 @@
+# about - about command
+
+echo "MeteorJS Comamnd Line Interface - Zio Software Development (Zio SD)"
+echo "Gary Ascuy <gary.ascuy@gmail.com>"
+date -u
+printf '\n'
+
+echo "For mode details find:"
+echo "  https://hub.docker.com/u/ziosd/"
+echo "  https://github.com/Gary-Ascuy/meteorjs"
+
+
