c70a268a6f720c4a00852aafcef941debb7b5697
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-11-22 00:07:05 -0400

    Adding Alpine generator

--- cli/plugins/meteor/alpinize.sh
+++ cli/plugins/meteor/alpinize.sh
@@ -1,3 +1,19 @@
-METEORJS_BUILD_NAME=$1
+set -e
 
-cat base | sed "s/__BUILD__/${METEORJS_BUILD_NAME}.alpine/g" > /meteor/Dockerfile
+METEORJS_BUILD_NAME=$1
+METEORJS_BUILD_NAME_ALPINE=${METEORJS_BUILD_NAME}.alpine
+
+# Alpine build & install meteor
+ls /meteor
+tar -xzf /meteor/${METEORJS_BUILD_NAME}.tar.gz
+# (cd bundle/programs/server/ && npm i && npm run install)
+# tar -czf /meteor/${METEORJS_BUILD_NAME_ALPINE} /meteor/
+
+# Dockerfile
+cat base | sed "s/__BUILD__/${METEORJS_BUILD_NAME_ALPINE}/g" > /meteor/Dockerfile
+
+# Message
+echo "Run following command to create the image:"
+echo "  $ docker build -t ${METEORJS_BUILD_NAME} ."
+printf "\n"
+echo "Successful alpine build created !!!"
